0fefc23cca41ab7dd8629293575c49c94f0f11f5
lopof <thbaumann90@gmail.com>
2020-10-01 06:42:12 +0000

    update velero

--- velero.sh
+++ velero.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+curl -m 5 -s https://github.com/vmware-tanzu/velero/releases/ > velero.txt
+
 cat velero.txt | grep -o -E '/vmware-tanzu/velero/releases/tag/.{0,6}' | cut -c 35- | head -1
+
+rm velero.txt
